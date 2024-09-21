b24a1fda76e7fe02f0f13e56a6abba496ede3548
myshy13 <hamish.olliver@icloud.com>
2024-09-21 19:11:31 +1000

    chnaged icons and added new identifier

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -4,7 +4,9 @@ function Sidebar() {
 	return (
 		<>
 			<div className="SideBar" id="SideBar">
-				<button className="btn btn-icon">+</button>
+				<button className="btn btn-icon" id="add-button">
+					+
+				</button>
 				<SideBarItem name="item" />
 			</div>
 		</>
